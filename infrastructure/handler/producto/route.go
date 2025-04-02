085e8208188d2ee479dface90a916d497653456f
ninosistemas10 <ninoarangotinco@gmail.com>
2025-04-02 17:50:41 +0200

    mi quinto commit

--- infrastructure/handler/producto/route.go
+++ infrastructure/handler/producto/route.go
@@ -29,7 +29,7 @@ func adminRoutes(e *echo.Echo, h handler, middlewares ...echo.MiddlewareFunc) {
 	route.POST("", h.Create)
 	route.PUT("/:id", h.Update)
 	route.DELETE("/:id", h.Delete)
-	route.PUT("/upDateNoImagen/:id", h.UpdateEsceptImage)
+
 	route.GET("", h.GetAll)
 	//route.GET("/:id", h.GetByID)
 }
@@ -40,5 +40,6 @@ func publicRoutes(e *echo.Echo, h handler) {
 	route.POST("", h.Create)
 	route.GET("", h.GetAll)
 	route.GET("/:id", h.GetByID)
+	route.PUT("/imagen/:id", h.UpdateImage)
 	route.GET("/categoria/:idcategoria", h.GetByCategoryID)
 }
